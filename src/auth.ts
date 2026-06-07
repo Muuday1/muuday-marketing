980193953ab6aebd34c1a1a8b7f4ac9f409f86e9
Muuday1 <igor@muuday.com>
2026-06-07 09:20:53 +0100

    fix: add explicit authorization params to GoogleProvider

--- src/auth.ts
+++ src/auth.ts
@@ -10,6 +10,13 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
     }),
     CredentialsProvider({
       name: 'password',
